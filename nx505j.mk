f18e1b6f32baf0e983e010999bb64ba1d6895aa4
Yukiww233 <624104679@qq.com>
2016-01-16 20:32:19 +0800

    deltest

--- nx505j.mk
+++ nx505j.mk
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/nubia/nx505j/nx505j-vendor.mk)
 
